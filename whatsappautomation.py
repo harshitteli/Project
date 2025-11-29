0c7a9688d6be85f18375befb860024e04248ef48
harshitteli <harshitsahu048@gmail.com>
2025-11-29 18:26:40 +0530

    Create whatsappautomation.py

---
+++ whatsappautomation.py
@@ -0,0 +1,4 @@
+import pywhatkit
+pywhatkit.sendwhatmsg_instantly("+919694452002","Hi its me")
+pywhatkit.sendwhatmsg("+919694452002","Hi its me",10,20)
+pywhatkit.sendwhatmsg_to_group("KYVew3VgcgeKyJfKn1iaDm","Hi its me",19,8)
