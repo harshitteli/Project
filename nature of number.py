4fd6e032d5bc122e365b47fd6b255c3854788abe
harshitteli <harshitsahu048@gmail.com>
2025-11-29 18:20:24 +0530

    Create nature of number.py

---
+++ nature of number.py
@@ -0,0 +1,8 @@
+number=float(input("enter the number"))
+if number>0:
+    print(f"{number} is a positive number")
+else:
+  if number<0:
+        print(f"{number} is a negative number")
+  else:
+        print("this is zero")
